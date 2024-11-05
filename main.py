cb4fd7780213bc878ab9c84eb84e2a514aafce57
henriquebrnetto <henriquebrnetto02@gmail.com>
2024-11-05 19:29:03 -0300

    Documentação

--- main.py
+++ main.py
@@ -4,31 +4,29 @@ import numpy as np
 
 
 
-forca = JogoDeForca()
-tam = forca.novo_jogo()
-jogador = Jogador(tam)
+forca = JogoDeForca() # Inicializa o jogo da forca
+tam = forca.novo_jogo() # Cria nova partida, escolhe a palavra e salva o tamanho da palavra
+jogador = Jogador(tam) # Inicializa um jogador apenas com as palavras daquele tamanho
 
 init = False
 palavras_certas = 0
-while forca.vidas > 0:
+while forca.vidas > 0: # Enquanto o jogador não errar
     
-    if init:
+    if init: # Caso o jogador tenha acertado a palavra, gera um novo jogador e nova partida
         tam = forca.novo_jogo()
         jogador = Jogador(tam)
         init = False
 
-    letra = jogador.guess_letter()
-    idxs = forca.tentar_letra(letra)
+    letra = jogador.guess_letter() # Jogador escolhe letra
+    idxs = forca.tentar_letra(letra) # Indexes onde aquela letra está presente na palavra
 
-    jogador.filter_palavras(letra, idxs)
+    jogador.filter_palavras(letra, idxs) # Reduz o vocabulário de acordo com a informação de indexes obtida
 
-    if ~np.any(jogador.palavra == ''):
-        if forca.tentar_palavra(''.join(jogador.palavra)):
+    if ~np.any(jogador.palavra == ''): # Se o array da palavra do jogador estiver preenchida
+        if forca.tentar_palavra(''.join(jogador.palavra)): # Se o jogador acertar a palavra
             palavras_certas += 1
             init = True
             print(f'Você já acertou {palavras_certas} palavras, e possui {forca.vidas} vidas.')
-        else:
-            pass
 
 print()
 print('Acabou o jogo!')
